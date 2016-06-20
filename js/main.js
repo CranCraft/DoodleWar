1868471979887a9450df5d5c5b9ea325d0cfb3dd
CranCraft <felix_ohlsen@yahoo.de>
2016-06-20 21:52:33 +0200

    Walls immovable

--- js/main.js
+++ js/main.js
@@ -184,15 +184,14 @@ function create() {
 	boxes.setAll('checkWorldBounds', true);
 
 	// Power Up Walls
-	walls = game.add.group();
-	walls.enableBody = true;
-	walls.physicsBodyType = Phaser.Physics.ARCADE;
+	walls = game.add.physicsGroup(Phaser.Physics.ARCADE);
 	walls.createMultiple(10, 'box');
 	walls.setAll('outOfBoundKill', true);
 	walls.setAll('checkWorldBounds', true);
 	walls.setAll('type', 2);
-    game.physics.arcade.enable(walls);
-
+	walls.setAll('enableBody', true);
+	walls.setAll('body.immovable', true);
+	
 	// Leben für Spieler 1
 	livesPlayer1 = game.add.group();
 
@@ -267,13 +266,17 @@ function update() {
 	if (game.physics.arcade.collide(player1, line1, null)
 			&& game.physics.arcade.collide(player2, line2, null)
 			&& game.physics.arcade.collide(player1, invisWall, null)
-			&& game.physics.arcade.collide(player2, invisWall, null)) {
+			&& game.physics.arcade.collide(player2, invisWall, null)
+			&& game.physics.arcade.collide(player1, walls, null)
+			&& game.physics.arcade.collide(player2, walls, null)) {
 
 	}
 	if ((game.physics.arcade.collide(player2, line2, null)
 			|| game.physics.arcade.collide(player1, line1, null)
-			|| game.physics.arcade.collide(player2, invisWall, null) || game.physics.arcade
-			.collide(player1, invisWall, null))) {
+			|| game.physics.arcade.collide(player2, invisWall, null) 
+			|| game.physics.arcade.collide(player1, invisWall, null)
+			|| game.physics.arcade.collide(player1, walls, null)
+			|| game.physics.arcade.collide(player2, walls, null))) {
 
 	}
 
